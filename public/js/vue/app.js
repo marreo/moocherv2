6524245d7a7ce3ddcd767f8db0d51ef7e88b49bb
Martin Lindgren <martin@marreomedia.se>
2017-06-10 17:04:03 +0200

    Lists all activities.

--- public/js/vue/app.js
+++ public/js/vue/app.js
@@ -1,4 +1,17 @@
 var app = new Vue({
+    mounted: function(argument) {
+        console.log("app is ready");
+    },
+    created() {
+        this.fetchData()
+    },
     el: '#app',
-    data: { message: 'Hello Vue!', title: 'Hejsa?' }
+    data: { message: 'Hello Vue!', activities: '' },
+    methods: {
+        fetchData() {
+            this.$http.get('/api/activities/get').then(response => {
+                this.activities.concat(response.body);
+            });
+        }
+    }
 })

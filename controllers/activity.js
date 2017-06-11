8b57720c817c22225402ff1b32700a2b377324a9
Kargnus <mange_johanson@hotmail.com>
2017-06-11 23:25:59 +0200

    activity.js - Fixed activity get method. Use Populate() to join/fetch related data. activities.pug - Also fixed conditional rendering if profile exists or not

--- controllers/activity.js
+++ controllers/activity.js
@@ -8,27 +8,11 @@ exports.index = (req, res) => {
     });
 };
 
-// async function populateModel(doc) {
-//     console.log('populating??' + doc);
-//     var activities = [];
-//     for (var act of doc) {
-//         console.log('bajsjdj' + act);
-//         User.findOne({ _id: act.turn }, function(err, usr) {
-//             console.log('found user?');
-//             act.turn = usr;
-//             activities.push(act);
-//         });
-//     }
-//     console.log('populateModel:' + activities);
-//     return activities;
-// };
-
 exports.getActivity = (req, res) => {
     return Activity.find({ users: req.user.id })
+        .populate("users")
+        .populate("turn")
         .then((doc) => {
-            // console.log('Found activity?');
-            // var activities = await populateModel(doc);
-            // console.log('asdsa' + activities);
             res.json(doc);
         });
 };

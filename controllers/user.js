d93066c11cb8780bb8356c5e322257ee22d05cd3
Martin Lindgren <martinmarklind@hotmail.com>
2017-06-13 21:35:30 +0200

    Removed unused api.

--- controllers/user.js
+++ controllers/user.js
@@ -195,24 +195,6 @@ exports.postDeleteAccount = (req, res, next) => {
 };
 
 /**
- * GET /account/unlink/:provider
- * Unlink OAuth provider.
- */
-exports.getOauthUnlink = (req, res, next) => {
-    const provider = req.params.provider;
-    User.findById(req.user.id, (err, user) => {
-        if (err) { return next(err); }
-        user[provider] = undefined;
-        user.tokens = user.tokens.filter(token => token.kind !== provider);
-        user.save((err) => {
-            if (err) { return next(err); }
-            req.flash('info', { msg: `${provider} account has been unlinked.` });
-            res.redirect('/account');
-        });
-    });
-};
-
-/**
  * GET /reset/:token
  * Reset Password page.
  */

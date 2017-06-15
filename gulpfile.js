2b06d16ee557eb72cfc9225a87370e363576deca
Martin Lindgren <martinmarklind@hotmail.com>
2017-06-15 22:08:29 +0200

    Updated bootstrap to 4-alpha.6

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var rename = require('gulp-rename');
 
-gulp.task('sass', function() {
-    return gulp.src('./public/css/main.scss')
+
+gulp.task('js', function () {
+    return gulp.src([
+        './node-modules/tether/dist/js/tether.min.js',
+        './node_modules/bootstrap/dist/js/bootstrap.min.js'
+        ])
+        .pipe(rename('modules.js'))
+        .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src([,
+        './public/css/main.scss'
+        ])
         .pipe(sass().on('error', sass.logError))
+        .pipe(rename('main.css'))
         .pipe(gulp.dest('./public/css/'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['js', 'sass']);

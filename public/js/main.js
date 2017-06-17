89b3670cad1ee619826129690243bae1a44f7621
Martin Lindgren <martinmarklind@hotmail.com>
2017-06-18 01:14:47 +0200

    Larger activity. Icon for closing an activity.

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
   // Place JavaScript code here...
 
-    $(document).on("click",".activity",(function() {
-      $(this).toggleClass('active');
+    $(document).on("click",".wrapper-center",(function() {
+      $(this).closest('.activity').addClass('active');
+    }));
+    $(document).on("click",".activity-close",(function() {
+      $(this).closest('.activity').removeClass('active');
     }));
 });
 

19f0e0080f711e3e497ca64ff7b9af2ace124ed1
Martin Lindgren <martinmarklind@hotmail.com>
2017-06-24 18:02:51 +0200

    Writing out version i footer.

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const multer = require('multer');
 
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
+
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
